cee70fae1b2fda48e3fe62ffaa5b0f63375a74d6
Thomas Boerger <thomas@webhippie.de>
2025-09-11 15:54:19 +0200

    chore: update flake config

--- flake.nix
+++ flake.nix
@@ -13,9 +13,14 @@
     flake-parts = {
       url = "github:hercules-ci/flake-parts";
     };
+
+    git-hooks = {
+      url = "github:cachix/git-hooks.nix";
+    };
   };
 
-  outputs = inputs@{ flake-parts, ... }:
+  outputs =
+    inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       imports = [
         inputs.devenv.flakeModule
@@ -28,36 +33,64 @@
         "aarch64-darwin"
       ];
 
-      perSystem = { config, self', inputs', pkgs, system, ... }: {
-        imports = [
-          {
-            _module.args.pkgs = import inputs.nixpkgs {
-              inherit system;
-              config.allowUnfree = true;
-            };
-          }
-        ];
-
-        devenv = {
-          shells = {
-            default = {
-              languages = {
-                terraform = {
-                  enable = true;
-                  package = pkgs.terraform;
-                };
-                ruby = {
-                  enable = true;
-                  package = pkgs.ruby_3_3;
-                };
+      perSystem =
+        {
+          config,
+          self',
+          inputs',
+          pkgs,
+          system,
+          ...
+        }:
+        {
+          imports = [
+            {
+              _module.args.pkgs = import inputs.nixpkgs {
+                inherit system;
+                config.allowUnfree = true;
               };
+            }
+          ];
 
-              packages = with pkgs; [
-                shellcheck
-              ];
+          devenv = {
+            shells = {
+              default = {
+                git-hooks = {
+                  hooks = {
+                    nixfmt-rfc-style = {
+                      enable = true;
+                    };
+
+                    terraform-format = {
+                      enable = true;
+                      package = pkgs.terraform;
+                    };
+
+                    tflint = {
+                      enable = true;
+                    };
+                  };
+                };
+
+                languages = {
+                  terraform = {
+                    enable = true;
+                    package = pkgs.terraform;
+                  };
+                  ruby = {
+                    enable = true;
+                    package = pkgs.ruby_3_4;
+                  };
+                };
+
+                packages = with pkgs; [
+                  _1password-cli
+                  cosign
+                  nixfmt-rfc-style
+                ];
+              };
             };
           };
         };
-      };
     };
 }
